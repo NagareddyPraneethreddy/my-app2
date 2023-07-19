131e7085a68d2e5466f7bb9eff22ceeffcee3c0c
NagareddyPraneethreddy <praneethreddy685@gmail.com>
2023-07-19 13:04:37 +0530

    created Form Array in this path

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
@@ -7,24 +7,40 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./create-user.component.scss']
 })
 export class CreateUserComponent {
-  
-  public userForm:FormGroup=new FormGroup({
-    name:new FormControl(),
-    age:new FormControl(),
-    phone:new FormControl(),
-    email:new FormControl(),
-    address:new FormGroup({
-      Hno:new FormControl(),
-      State:new FormControl(),
-      pin:new FormControl(),
+
+  public userForm: FormGroup = new FormGroup({
+    name: new FormControl(),
+    age: new FormControl(),
+    phone: new FormControl(),
+    email: new FormControl(),
+    address: new FormGroup({
+      Hno: new FormControl(),
+      State: new FormControl(),
+      pin: new FormControl(),
     }),
     type: new FormControl(),
-    busFee:new FormControl(),
-    hostelFee:new FormControl(),
-    
+    busFee: new FormControl(),
+    hostelFee: new FormControl(),
+    cards: new FormArray([]),
   });
-  submit(){
+
+get cardsFormArray(){
+  return this.userForm.get('cards')as FormArray;
+}
+
+addcard(){
+  this.cardsFormArray.push(
+    new FormGroup({
+      no: new FormControl(),
+      exp:new FormControl(),
+      cvv: new FormControl(),
+    })
+  )
+}
+  submit() {
     console.log(this.userForm);
   }
-
+  deletecard(i:number){
+    this.cardsFormArray.removeAt(i);
+  }
 }
